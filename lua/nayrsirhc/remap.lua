ea1051810b6eac8a6d9316f4bf43eacd783c8704
Christopher Ryan <christopher.ryan@investec.co.za>
2023-04-21 23:41:12 +0200

    feat: add remap to yank into system clipboard

--- lua/nayrsirhc/remap.lua
+++ lua/nayrsirhc/remap.lua
@@ -5,3 +5,9 @@ vim.keymap.set("n","<leader>ps", vim.cmd.PackerSync)
 -- Move magic visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Yank into system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
