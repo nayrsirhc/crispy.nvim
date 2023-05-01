c82319b6d1bc886917af31135b0b7c3e73fc5dd5
Christopher Ryan <christopher.ryan@investec.co.za>
2023-05-01 22:37:54 +0200

    feat: add docui

---
+++ lua/nayrsirhc/docui.lua
@@ -0,0 +1,20 @@
+local Terminal = require("toggleterm.terminal").Terminal
+
+-- Docker
+local docker_tui = "docui"
+
+local Docui_client = Terminal:new {
+  cmd = docker_tui,
+  dir = "git_dir",
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+}
+
+function Docui_client_toggle()
+  Docui_client:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>dui", "<cmd>lua Docui_client_toggle()<CR>", {noremap = true, silent = true, desc = "Toggle Docui"})
